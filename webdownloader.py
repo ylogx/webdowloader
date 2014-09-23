2a1b0607863ac8a3c581e65fa8fbd7cfc42af9d4
Shubham Chaudhary <me@shubhamchaudhary.in>
2014-09-23 22:54:48 +0530

    Some fixups

--- webdownloader.py
+++ webdownloader.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 #
-#  webdownloader.py - download all data with specific extensions linked
-#  in any html page, useful for course websites like ocw.mit.org etc.
-#
 #  Copyright (c) 2014 Shubham Chaudhary <me@shubhamchaudhary.in>
 #
 #  This program is free software: you can redistribute it and/or modify
@@ -19,7 +16,12 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import sys, os, re
+''' webdownloader.py - download all data with specific extensions linked
+    in any html page, useful for course websites like ocw.mit.org etc. '''
+
+import sys
+#import os
+#import re
 
 if sys.version_info >= (3,):
     import urllib.request as urllib2
@@ -40,17 +42,17 @@ except ImportError:
 
 DEFAULT_DOWNLOAD_TYPE = 'jpg'
 DEFAULT_DESTINATION_FOLDER = './'
-IMAGE_EXTENSIONS = [ 'jpg', 'jpeg', 'png', 'gif' ]
-DOCUMENT_EXTENSIONS = [ 'pdf', 'doc', 'docx' ]
-AUDIO_EXTENSIONS = [ 'mp3' ]
-VIDEO_EXTENSIONS = [ 'mp4', 'webm', '3gp', 'mov', 'mkv' ]
+IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif']
+DOCUMENT_EXTENSIONS = ['pdf', 'doc', 'docx']
+AUDIO_EXTENSIONS = ['mp3']
+VIDEO_EXTENSIONS = ['mp4', 'webm', '3gp', 'mov', 'mkv']
 
 try:
     from filedownloader.file_downloader import download, download_list
 except ImportError:
     print('Dependency Problem:')
     print('Download file_downloader.py in the same folder')
-    print('Type: `wget https://raw.githubusercontent.com/shubhamchaudhary/python/master/file_downloader.py`')
+    print('Type: `wget https://raw.githubusercontent.com/shubhamchaudhary/filedownloader/master/file_downloader.py`')
     sys.exit(1)
 
 def get_href(url):
@@ -63,8 +65,9 @@ def get_href(url):
         urllist.append(address)
     return urllist
 
-def parse(url,extension=None,destination=None):
-    assert(isinstance(url,str))
+def parse(url, extension=None, destination=None):
+    ''' Parse the given url for extension and store them at destination '''
+    assert isinstance(url, str)
     if extension == None:
         extension = DEFAULT_DOWNLOAD_TYPE
     if destination == None:
@@ -73,40 +76,58 @@ def parse(url,extension=None,destination=None):
     urllist = []
     href_list = get_href(url)
     for address in href_list:
-        if (not address): continue      #If address in NoneType
-        if (address[-len(extension):] == extension or address[-4:] == 'jpeg' or address[-3:] == 'png'):
-            print('Found ',extension,': ',address)
-            urllist.append(urlparse.urljoin(url,address))
-    print('Total',len(urllist),'files found with extension:', extension)
-    download_list(urllist,destination)
+        if not address:
+            continue      #If address in NoneType
+        if (address[-len(extension):] == extension
+                or address[-4:] == 'jpeg'
+                or address[-3:] == 'png'):
+            print('Found ', extension, ': ', address)
+            urllist.append(urlparse.urljoin(url, address))
+    print('Total', len(urllist), 'files found with extension:', extension)
+    download_list(urllist, destination)
 
-def parse_filetype(url,filetype,destination):
+def parse_filetype(url, filetype, destination):
     ''' Parse files with specific filetype
     e.g webdownloader -t image -d new_folder http://url.com/page '''
     filetype = filetype.lower()
-    if filetype == 'i' or filetype == 'image' or filetype == 'images' or filetype == 'img':
+    if (filetype == 'i'
+            or filetype == 'image'
+            or filetype == 'images'
+            or filetype == 'img'):
         EXTENSIONS = IMAGE_EXTENSIONS
-    elif filetype == 'd' or filetype == 'doc' or filetype == 'pdf':
+    elif (filetype == 'd'
+            or filetype == 'doc'
+            or filetype == 'pdf'):
         EXTENSIONS = DOCUMENT_EXTENSIONS
-    elif filetype == 'v' or filetype == 'video' or filetype == 'videos' or filetype == 'movie' or filetype == 'movies':
+    elif (filetype == 'v'
+            or filetype == 'video'
+            or filetype == 'videos'
+            or filetype == 'movie'
+            or filetype == 'movies'):
         EXTENSIONS = VIDEO_EXTENSIONS
-    elif filetype == 'a' or filetype == 'audio' or filetype == 'sound':
+    elif (filetype == 'a'
+            or filetype == 'audio'
+            or filetype == 'sound'):
         EXTENSIONS = AUDIO_EXTENSIONS
     for extension in EXTENSIONS:
         print('Downloading {0} filetype:'.format(extension))
-        parse(url,extension,destination)
+        parse(url, extension, destination)
 
 def main(argv):
+    ''' The main function '''
     from optparse import OptionParser
     parser = OptionParser()
     parser.add_option("-e", "--extension", dest="extension",
-                     help="Download files with this extension .EXT", metavar="EXT")
+                     help="Download files with this extension .EXT",
+                     metavar="EXT")
     parser.add_option("-d", "--destination", dest="destination",
-                     help="destination FOLDER where files will be saved", metavar="FOLDER")
+                     help="destination FOLDER where files will be saved",
+                     metavar="FOLDER")
     parser.add_option("-f", "--file", dest="filename",
                      help="parse a downloaded html FILE", metavar="FILE")
     parser.add_option("-t", "--type", dest="filetype",
-                     help="Download files of this filetype: e.g image, videos, doc")
+                     help="Download files of this filetype: e.g image, videos,\
+                             doc")
     parser.add_option("-q", "--quiet",
                      action="store_false", dest="verbose", default=True,
                      help="don't print status messages to stdout")
@@ -115,12 +136,12 @@ def main(argv):
 
     if args:    #args[0] is the url
         if options.filetype:
-            parse_filetype(args[0],options.filetype,options.destination)
+            parse_filetype(args[0], options.filetype, options.destination)
         else:
             for anurl in args:
-                print('Parsing: ',anurl)
+                print('Parsing: ', anurl)
                 try:
-                    parse(anurl,options.extension,options.destination)
+                    parse(anurl, options.extension, options.destination)
                 except:
                     #print('Error encountered with this page.  Details:',
                             #file=sys.stderr)
@@ -138,4 +159,5 @@ if __name__ == '__main__':
     except ValueError:
         print(sys.exc_info()[1])
     except KeyboardInterrupt:
-        print('\nGracefully exiting, but that is not fair. I was trying to get it for you :(')
+        print('\nGracefully exiting, but that is not fair. \
+                I was trying to get it for you :(')
